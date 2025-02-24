bdb094e231eb0fb3dfbca59fc80a48a428838308
Pasindu shanuka <143288490+PShanuKa@users.noreply.github.com>
2025-02-24 14:23:51 +0530

    subscription

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-import express from 'express';
-import axios from 'axios'; 
+import express from "express";
+import axios from "axios";
 const app = express();
 const port = 3000;
-import cors from 'cors';
+import cors from "cors";
 
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-
 app.use(express.json());
-app.use(cors({
-  origin: '*',
-}));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-
-app.get('/', (req, res) => {
-  res.send('App is running');
+app.get("/", (req, res) => {
+  res.send("App is running");
 });
 
-
-app.get('/token', async (req, res) => {
+app.get("/token", async (req, res) => {
   try {
     const response = await axios.post(
       "https://login.microsoftonline.com/fa51c2d9-b92c-4ee0-8186-a8733f338c47/oauth2/v2.0/token",
@@ -39,17 +38,40 @@ app.get('/token', async (req, res) => {
     const data = response.data;
 
     if (data.access_token) {
-
       res.json({ success: true, access_token: data.access_token });
     } else {
-      res.status(400).json({ success: false, message: "No access token received" });
+      res
+        .status(400)
+        .json({ success: false, message: "No access token received" });
     }
   } catch (error) {
     console.error("Error refreshing token:", error.message);
-    res.status(500).json({ success: false, message: "Failed to refresh token" });
+    res
+      .status(500)
+      .json({ success: false, message: "Failed to refresh token" });
   }
 });
 
+app.get("/subscription/:token", async (req, res) => {
+  if (
+    token ===
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+  ) {
+
+    const data = {
+      subscriptionId: "1234567890",
+      availableDate: "2",
+      access : true
+
+    }
+    res.json({ success: true, message: "Subscription data", data });
+  } else {
+    const data = {
+      access: false,
+    };
+    res.json({ success: false, message: "Invalid token", data });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
